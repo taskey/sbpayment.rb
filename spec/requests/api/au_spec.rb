bc3b2a2db2f516b66c2b8c5f854f57ad7dd1c289
tashiro <33741858+tashirosota@users.noreply.github.com>
2021-02-05 16:11:15 +0900

    ST01-00104-402 auかんたん決済 購入要求要求処理 (継続課金(定期・従量)) (#94)
    * feature Sbpayment::API::Au::AuthorizationRequest
    
    * add type option to Au::AuthorizationRequest key

--- spec/requests/api/au_spec.rb
+++ spec/requests/api/au_spec.rb
@@ -1,5 +1,76 @@
 require 'spec_helper'
 
 describe 'au API behavior' do
-  pending 'Needs tracking_id from link type'
+  describe 'AuthorizationRequest' do
+    around do |e|
+      VCR.use_cassette 'au-authorization' do
+        e.run
+      end
+    end
+
+    before do
+      Sbpayment.configure do |x|
+        x.sandbox = true
+        x.merchant_id = '30132'
+        x.service_id  = '103'
+        x.basic_auth_user     = '30132103'
+        x.basic_auth_password = 'ed679e1c9f90c2ab96b25d5c580b58e25192eb5d'
+        x.hashkey             = 'ed679e1c9f90c2ab96b25d5c580b58e25192eb5d'
+        x.cipher_code         = 'ed679e1c9f90c2ab96b25d5c'
+        x.cipher_iv           = '580b58e2'
+      end
+      @req = Sbpayment::API::Au::AuthorizationRequest.new
+      @req.cust_code        = SecureRandom.hex
+      @req.order_id         = SecureRandom.hex
+      @req.item_id          = 'item_2'
+      @req.item_name        = '継続課金購入'
+      @req.amount           = 980
+    end
+
+    describe '#attributes' do
+      it 'keys' do
+        expected_json_user_keys = %w(
+          merchant_id
+          service_id
+          tracking_id
+          cust_code
+          order_id
+          item_id
+          item_name
+          tax
+          amount
+          free1
+          free2
+          free3
+          order_rowno
+          request_date
+          limit_second
+          sps_hashcode
+        )
+        expect(@req.attributes.keys).to eq expected_json_user_keys
+      end
+    end
+
+    describe '#update_sps_hashcode' do
+      it do
+        expect(@req.sps_hashcode).to eq ''
+        hash_code = @req.update_sps_hashcode
+        expect(@req.sps_hashcode).to eq hash_code
+      end
+    end
+
+    describe '#perform' do
+      context 'success' do
+        # couldn't implement because trackng_id is nil
+      end
+
+      context 'failed' do
+        it do
+          @req.update_sps_hashcode
+          res = @req.perform
+          expect(res.error.class).to eq Sbpayment::API40203Error
+        end
+      end
+    end
+  end
 end

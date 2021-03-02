45a3a04cf33c78258ad8e07e667fb2e103ccf660
Shun Ozaki <ozaki.shun5@gmail.com>
2021-03-02 17:38:26 +0900

    Implement Sbpayment::API::Docomo::SimplifiedCancelRequest (#143)
    sps-api-request id is ST02-00302-401

---
+++ lib/sbpayment/api/docomo/simplified_cancel.rb
@@ -0,0 +1,22 @@
+require_relative '../../request'
+require_relative '../../response'
+
+module Sbpayment
+  module API
+    module Docomo
+      class SimplifiedCancelRequest < Request
+        tag 'sps-api-request', id: 'ST02-00302-401'
+        key :merchant_id, default: -> { Sbpayment.config.merchant_id }
+        key :service_id,  default: -> { Sbpayment.config.service_id }
+        key :tracking_id
+        key :processing_datetime
+        key :request_date, default: -> { TimeUtil.format_current_time }
+        key :limit_second
+        key :sps_hashcode
+      end
+
+      class SimplifiedCancelResponse < Response
+      end
+    end
+  end
+end
